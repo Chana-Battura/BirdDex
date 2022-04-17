96358a16474f6c4c72b870738f7c9612e1c45e9c
mahitoburrito <mahitraj@gmail.com>
2022-04-16 18:01:57 -0700

    test commit - mahit branch

--- birdDex_app/starting/models.py
+++ birdDex_app/starting/models.py
@@ -13,7 +13,7 @@ class User (models.Model):
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
     email = models.EmailField('User Email')
-    birds_found = finders = models.ManyToManyField(Bird, blank=True)
+    #birds_found = finders = models.ManyToManyField(Bird, blank=True)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
